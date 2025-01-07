1e900319061b9a4f30c997f1dcaf23216a53a13e
Ron Panduwana <panduwana@gmail.com>
2025-01-07 09:00:52 +0700

    Product search: add related products

--- index.py
+++ index.py
@@ -22,39 +22,11 @@ CORS(app, resources={
 static_dir = Path(app.static_folder)
 product_images_dir = static_dir / 'products'
 product_images_relpath = static_dir.name + '/products/'
-files = [f for f in os.listdir(product_images_dir)
+imagefiles = [f for f in os.listdir(product_images_dir)
     if os.path.isfile(os.path.join(product_images_dir, f))
     and not f.startswith('.')
 ]
-files.sort()
-
-
-def product(brand, item):
-    return {
-        'name' : hydrate(item.filename, brand),
-        'image' : {
-            'src' : item.relpath,
-            'width' : item.width,
-            'height' : item.height,
-        },
-    }
-
-def hydrate(filename, brand):
-    return filename[4:].split('.')[0] \
-        .replace('X', brand) \
-        .replace('~' + brand + '~', brand.lower()) \
-        .replace('~' + brand, brand.lower()) \
-        .replace(brand + '~', brand.title()) \
-        .replace('_', ' ')
-
-def pattern_from(filename):
-    regex = '(\\S+)'
-    scrub = filename.strip().replace('-', ' ').replace('(', '').replace(')', '')
-    pattern = hydrate(scrub, '(regex)').lower().replace('(regex)', regex)
-    if '~' in filename:
-        pattern = [part for part in pattern.split(' ') if regex in part][0]
-
-    return re.compile(pattern)
+imagefiles.sort()
 
 def image_info(filename):
     with Image.open(product_images_dir / filename) as image:
@@ -62,16 +34,49 @@ def image_info(filename):
         return SimpleNamespace(
             filename=filename,
             relpath=product_images_relpath + filename,
-            pattern=pattern_from(filename),
+            regex=regexify(filename),
             width=width,
             height=height,
         )
-files = [image_info(f) for f in files]
+imagefiles = map(image_info, imagefiles)
 
+
+def product(brand, item):
+    name = brandify(item.filename, brand)
+    return {
+        'name' : name,
+        'tags' : tagify(name),
+        'image' : {
+            'src' : item.relpath,
+            'width' : item.width,
+            'height' : item.height,
+        },
+    }
+
+def denoise(string):
+    return string.replace('-', ' ').replace('(', '').replace(')', '')
+
+def tagify(string):
+    return denoise(string).lower()
+
+def brandify(filename, brand):
+    return filename[4:].split('.')[0] \
+        .replace('X', brand) \
+        .replace('~' + brand + '~', brand.lower()) \
+        .replace('~' + brand, brand.lower()) \
+        .replace(brand + '~', brand.title()) \
+        .replace('_', ' ')
+
+def regexify(filename):
+    regex = '(\\S+)'
+    pattern = brandify(denoise(filename), '(regex)').lower().replace('(regex)', regex)
+    if '~' in filename:
+        pattern = [part for part in pattern.split(' ') if regex in part][0]
+    return re.compile(pattern)
 
 def catalog_for(brand):
-    if brand == 'dhc':
-        return [files[f] for f in [12, 58, 32, 5, 8]]
+    if brand == 'DHC':
+        return [imagefiles[f] for f in [12, 58, 32, 5, 8]]
 
     if len(brand) < 3:
         return []
@@ -79,36 +84,47 @@ def catalog_for(brand):
     if ' ' in brand:
         return []
 
-    num_items = ord(brand[0]) - ord('a') + 1 # A=1 B=2 etc
+    num_items = ord(brand[0]) - ord('A') + 1 # A=1 B=2 etc
     if num_items < 1 or num_items > 26:
         return []
 
-    catalog = files[:]
+    catalog = imagefiles[:]
     random.seed(int.from_bytes(hashlib.md5(brand.encode()).digest()))
     random.shuffle(catalog)
     return catalog[:num_items]
 
-
 def lookup(query):
-    query = query.strip().lower().replace('-', ' ')
+    query = tagify(query)
     brand = query
     reverse_matches = set([])
     if len(query) > 3:
-        for f in files:
-            found = f.pattern.search(query)
+        for f in imagefiles:
+            found = f.regex.search(query)
             if found:
-                brand = found.group(1)
                 reverse_matches.add(f.filename)
-
-    catalog = catalog_for(brand)
-    matches = [item for item in catalog if item.filename in reverse_matches]
-    if not matches and brand != query:
-        return []
+                capture = found.group(1)
+                if len(capture) < len(brand):
+                    brand = capture
 
     brand = brand.title() if len(brand) > 3 else brand.upper()
+    catalog = catalog_for(brand)
+    matches = [item for item in catalog if item.filename in reverse_matches]
     products = [product(brand, item) for item in matches or catalog]
-    products.sort(key=lambda p: p['name'])
+    fullmatches = [item for item in products if query in item['tags']]
+    if fullmatches:
+        return fullmatches
+
+    if ' ' in query:
+        parts = query.split(' ')
+        regex = '.*'.join(parts)
+        already_found = set([item['name'] for item in products])
+        related = [item for part in parts for item in lookup(part)
+            if re.search(regex, item['tags'])
+            and item['name'] not in already_found
+        ]
+        products.extend(related)
     return products
+
 
 @app.route('/api/products/')
 def api_products():
@@ -116,9 +132,7 @@ def api_products():
     if not query:
         return []
 
-    products = lookup(query)
-    if not products and ' ' in query:
-        products = [item for part in query.split(' ') for item in lookup(part) if query in item['name'].lower().replace('-', ' ')]
-        print([p['name'] for p in products])
-        
+    products = lookup(query.strip())
+    products.sort(key=lambda item: item['name'])
     return products
+
